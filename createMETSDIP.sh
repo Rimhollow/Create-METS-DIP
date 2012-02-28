238c27ce8058d3d54a487da9b5859cf718dc934d
rmholl0 <rmholl0@uky.edu>
2012-02-27 19:04:54 -0500

    Fully functional in Archivematica

--- createMETSDIP.sh
+++ createMETSDIP.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 set -e
 
-# Get the path of the zip file we're dealing with.
+echo "***** Create METS DIP shell script"
+echo "pwd:"
+pwd
+
+# Incoming parameters:
+# $1 relative location (working path)
+# $2 path to client scripts directory
+# $3 path to Solrize watch directory
+
+# Get the path of the zip file we're dealing with, 
+# and of the bag we're about to extract.
 # e.g. if the incoming path is /foo/bar/baz, the package is baz.
 ZIPNAME=$1/`basename $1`.zip
+BAGNAME=$1/`basename $1`
 
-# Everything in the zip is contained in a subdir with the same name as the
-# package; unzipping to the parent directory ensures that the contants don't
-# end up in a redundant subdirectory.
-ZIPTARGET=$1/..
-
-# Unzip and delete the zip file and pass the extracted bag to createMETSDIP.rb.
-unzip $ZIPNAME -d $ZIPTARGET
-rm $ZIPNAME
-ruby createMETSDIP.rb $1
+# Unzip the zip file, pass the extracted bag to createMETSDIP.rb,
+# and move the created METS DIP into the Solrize watch dir.
+unzip $ZIPNAME -d $1
+ruby $2/createMETSDIP.rb $BAGNAME
+mv $BAGNAME $3
